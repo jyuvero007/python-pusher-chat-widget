5730e428e19aa18a6f8a927abcc204962b4c3cda
jyuvero007 <70600786+jyuvero007@users.noreply.github.com>
2020-09-17 16:38:32 -0400

    Update admin.js
    Updated the keys.

--- static/js/admin.js
+++ static/js/admin.js
@@ -5,9 +5,9 @@
     // Configure Pusher instance
     // ----------------------------------------------------
 
-    var pusher = new Pusher('PUSHER_APP_KEY', {
+    var pusher = new Pusher('031a5f6199801fa8d257', {
         authEndpoint: '/pusher/auth',
-        cluster: 'PUSHER_APP_CLUSTER',
+        cluster: 'us2',
         encrypted: true
       });
 
